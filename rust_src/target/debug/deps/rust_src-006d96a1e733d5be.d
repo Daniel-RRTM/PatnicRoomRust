2347b2f28127f91591fd426af4170def8852a89c
daniel-rrtm <zeisler.daniel@protonmail.com>
2025-02-22 03:54:33 +0000

    initial

--- rust_src/target/debug/deps/rust_src-006d96a1e733d5be.d
+++
@@ -1,5 +0,0 @@
-/home/sudo/Schreibtisch/Projects/PatnicRoomRust/rust_src/target/debug/deps/librust_src-006d96a1e733d5be.rmeta: src/main.rs
-
-/home/sudo/Schreibtisch/Projects/PatnicRoomRust/rust_src/target/debug/deps/rust_src-006d96a1e733d5be.d: src/main.rs
-
-src/main.rs:
--- rust_src/target/debug/deps/rust_src-a27786de8e236491.d
+++
@@ -1,5 +0,0 @@
-/home/sudo/Schreibtisch/Projects/PatnicRoomRust/rust_src/target/debug/deps/librust_src-a27786de8e236491.rmeta: src/main.rs
-
-/home/sudo/Schreibtisch/Projects/PatnicRoomRust/rust_src/target/debug/deps/rust_src-a27786de8e236491.d: src/main.rs
-
-src/main.rs:
